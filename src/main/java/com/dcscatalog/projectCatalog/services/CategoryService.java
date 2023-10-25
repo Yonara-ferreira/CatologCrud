bad2cb23d420828e4a8c7066a8c9b899f80014b2
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2023-10-25 09:22:33 -0300

    Realize method findByIdCategories

--- src/main/java/com/dcscatalog/projectCatalog/services/CategoryService.java
+++ src/main/java/com/dcscatalog/projectCatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.dcscatalog.projectCatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,20 @@ public class CategoryService {
 		List<Category> categories = repository.findAll();
 		return categories.stream().map(category -> new CategoryDTO(category)).toList();
 	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findByIdCategory(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		
+		if(obj.isEmpty()) {throw new RuntimeException();}
+		
+		return new CategoryDTO(entity);
+	}
+	
+
+	
+	
 	
 	
 

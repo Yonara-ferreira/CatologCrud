7b9618e5739f90448c9862ad0c1ad05a960deb33
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2023-11-07 00:21:13 -0300

    method of deleting category

--- src/main/java/com/dcscatalog/projectCatalog/services/CategoryService.java
+++ src/main/java/com/dcscatalog/projectCatalog/services/CategoryService.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dcscatalog.projectCatalog.dto.CategoryDTO;
 import com.dcscatalog.projectCatalog.entities.Category;
 import com.dcscatalog.projectCatalog.repositories.CategoryRepository;
+import com.dcscatalog.projectCatalog.services.exceptions.DatabaseException;
 import com.dcscatalog.projectCatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -52,6 +55,20 @@ public class CategoryService {
 			throw new ResourceNotFoundException("id not found " + id );
 		}
 	}
+
+	public void deleteCategory(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException("id not found " + id);
+		}
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+
+	}
+	
+	
 	
 	
 		

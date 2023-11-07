611aa7cee81cf19dbd3ea21c14a5205cfc31f316
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2023-11-06 22:17:16 -0300

    Creating method post from category

--- src/main/java/com/dcscatalog/projectCatalog/resources/CategoryResource.java
+++ src/main/java/com/dcscatalog/projectCatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.dcscatalog.projectCatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dcscatalog.projectCatalog.dto.CategoryDTO;
 import com.dcscatalog.projectCatalog.services.CategoryService;
@@ -29,6 +33,14 @@ public class CategoryResource {
 	public ResponseEntity<CategoryDTO> findById(@PathVariable(value = "idCategory") Long idCategory){
 		CategoryDTO dto = service.findByIdCategory(idCategory);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping(value = "/newCategory")
+	public ResponseEntity<CategoryDTO> insertNewCategory(@RequestBody  CategoryDTO dto){
+		dto = service.insertNewCategory(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
--- src/main/java/com/dcscatalog/projectCatalog/services/CategoryService.java
+++ src/main/java/com/dcscatalog/projectCatalog/services/CategoryService.java
@@ -30,6 +30,14 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
+	@Transactional
+	public CategoryDTO insertNewCategory(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
 		
 
 }
